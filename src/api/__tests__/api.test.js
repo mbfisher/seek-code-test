268421df4b6bfb04610a9849d7515d3f410ef71a
Mike Fisher <mike@mbfisher.com>
2018-06-03 15:28:03 +1000

    invokeApi test abstraction

--- src/api/__tests__/api.test.js
+++ src/api/__tests__/api.test.js
@@ -2,27 +2,31 @@ import request from "supertest";
 import api from "../";
 
 describe("POST /checkout", () => {
-  test("accepts products are returns a total", async () => {
-    const customer = "default";
-    const items = ["classic"];
-
-    const response = await request(api)
-      .post("/checkout")
-      .send({
-        customer,
-        items
-      });
-
-    expect(response.statusCode).toBe(200);
-    expect(response.headers["content-type"]).toEqual(
-      "application/json; charset=utf-8"
-    );
-
-    const checkout = response.body;
-    console.info(checkout);
+  test("accepts items and returns a total", async () => {
+    const checkout = await invokeApi({
+      customer: "default",
+      items: ["classic"]
+    });
 
     expect(checkout.id).not.toBeNull();
-    expect(checkout.customer).toEqual(customer);
-    expect(checkout.items).toEqual(items);
+    expect(checkout.customer).toEqual("default");
+    expect(checkout.items).toEqual(["classic"]);
   });
 });
+
+const invokeApi = async ({ customer, items }) => {
+  const response = await request(api)
+    .post("/checkout")
+    .send({
+      customer,
+      items
+    });
+
+  expect(response.statusCode).toBe(200);
+  expect(response.headers["content-type"]).toEqual(
+    "application/json; charset=utf-8"
+  );
+
+  console.info(response.body);
+  return response.body;
+};
