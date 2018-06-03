7757257ba3bb21692879bfb06a6ca77e523edc88
Mike Fisher <mike@mbfisher.com>
2018-06-03 15:22:46 +1000

    Submit items to checkout API

--- src/api/__tests__/api.test.js
+++ src/api/__tests__/api.test.js
@@ -3,13 +3,18 @@ import api from "../";
 
 describe("POST /checkout", () => {
   test("accepts products are returns a total", async () => {
-    const response = await request(api).post("/checkout");
+    const items = ["classic"];
+
+    const response = await request(api)
+      .post("/checkout")
+      .send({ items });
+
     expect(response.statusCode).toBe(200);
     expect(response.headers["content-type"]).toEqual(
       "application/json; charset=utf-8"
     );
 
     console.info(response.body);
-    expect(response.body.items).toEqual([]);
+    expect(response.body.items).toEqual(items);
   });
 });
