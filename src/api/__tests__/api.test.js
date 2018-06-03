ad443bb8f664df25c4e36ba6e9d1da07488c067b
Mike Fisher <mike@mbfisher.com>
2018-06-03 15:32:26 +1000

    Red tests for privileged customers

--- src/api/__tests__/api.test.js
+++ src/api/__tests__/api.test.js
@@ -12,6 +12,37 @@ describe("POST /checkout", () => {
     expect(checkout.customer).toEqual("default");
     expect(checkout.items).toEqual(["classic"]);
   });
+
+  describe("privileged customers", () => {
+    test("default", async () =>
+      expect(
+        invokeApi({
+          customer: "default",
+          items: ["classic", "standout", "premium"]
+        }).total
+      ).toEqual(987.97));
+    test("unilever", async () =>
+      expect(
+        invokeApi({
+          customer: "unilever",
+          items: ["classic", "classic", "classic", "premium"]
+        }).total
+      ).toEqual(934.97));
+    test("apple", async () =>
+      expect(
+        invokeApi({
+          customer: "apple",
+          items: ["standout", "standout", "standout", "premium"]
+        }).total
+      ).toEqual(1294.96));
+    test("nike", async () =>
+      expect(
+        invokeApi({
+          customer: "nike",
+          items: ["premium", "premium", "premium", "premium"]
+        }).total
+      ).toEqual(1519.96));
+  });
 });
 
 const invokeApi = async ({ customer, items }) => {
